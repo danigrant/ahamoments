ae663d619eb0409c8e3639bb8a59d4ea6d8ef741
dani <mayor@dani.town>
2019-11-14 14:46:03 -0500

    not sure but apparently theres stuff to push

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cf241769507051a1aa47";
+/******/ 	var hotCurrentHash = "e6b43c719feb8c334e66";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
