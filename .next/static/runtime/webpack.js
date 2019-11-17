7bb94477c8bc3793e1a84944362c2aae74cecc15
dani <mayor@dani.town>
2019-11-17 08:26:31 -0500

    progress

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "28b58a6a3d779552101d";
+/******/ 	var hotCurrentHash = "d1343fdd3765e03a2b05";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
